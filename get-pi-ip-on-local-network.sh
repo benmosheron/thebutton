9b665863346d6a513238011e6703ddb5b39207e4
Ben Sheron <ben.sheron@cloudreach.com>
2017-10-11 22:20:24 +0100

    Script detects and sshs into pi

--- get-pi-ip-on-local-network.sh
+++ get-pi-ip-on-local-network.sh
@@ -1,2 +1,4 @@
-echo "ssh ben@" > ssh-ben.sh
+echo -n "ssh ben@" > ssh-ben.sh
 ping -c 1 raspberrypi.local | grep -oE "\d\d\d\.\d\d\d.\d\.\d\d\d" | tail -1 >> ssh-ben.sh
+chmod u+x ssh-ben.sh
+./ssh-ben.sh
